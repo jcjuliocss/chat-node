635d981618ac7d696064b8aae798c59539df44db
Julio Cesar <39426877+jcjuliocss@users.noreply.github.com>
2020-05-04 13:53:07 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', function(request, response){
+	response.render('index.html')
+})
+
+mensagens = []
+
+io.on('connection', function(socket){
+	console.log('Usuario conectado')
+
+	socket.emit('carrega_historico', mensagens)
+
+	socket.on('envia_mensagem', function(dados){
+		mensagens.push(dados)
+		socket.broadcast.emit('mensagem_recebida', dados)
+	})
+})
+
+server.listen(3000)
